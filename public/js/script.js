9a20622afdfa0d8521bff0efbfab26a267055aa0
Matthew Guillen <matthewguillen777@gmail.com>
2021-03-06 20:21:59 -0500

    added handlebars, home and post content

--- public/js/script.js
+++ public/js/script.js
@@ -1,68 +1,74 @@
-$(function () {
-    // Adds country list from json file to dropdown menu in sign-up
-    var countryOptions;
-    $.getJSON("/public/js/countries.json", function (result) {
-        $.each(result, function (i, country) {
-            //<option value="countrycode">contryname</option>
-            countryOptions += "<option value='" + country.code + "'>" + country.name + "</option>";
-        });
-        $("#country").html(countryOptions);
+/* eslint-disable prettier/prettier */
+$(() => {
+  // Adds country list from json file to dropdown menu in sign-up
+  let countryOptions;
+  $.getJSON("/public/js/countries.json", result => {
+    $.each(result, (i, country) => {
+      //<option value="countrycode">contryname</option>
+      countryOptions += "<option value='" + country.code + "'>" + country.name + "</option>";
     });
+    $("#country").html(countryOptions);
+  });
 
-    // Submit login
-    // $("#login").click(function (e) {
-    //     e.preventDefault();
-    //     location.href = "/public/dashboard.html";
-    // });
-    // // Submit sign-up
-    // $("#sign-up").click(function (e) {
-    //     e.preventDefault();
-    //     location.href = "/public/dashboard.html";
-    // });
-    
-    // Hide/Show navigation tab on user click in dashboard
-    $("#open-nav").click(function () {
-        $("#mySidenav").css("width", "250px");
-    })
-    $(".close-btn").click(function () {
-        $("#mySidenav").css("width", "0px");
-    })
+  // Submit login
+  $("#log-out-btn").click(() => {
+    location.href = "/";
+  });
+  // Submit sign-up
+  $("#sign-up").click(() => {
+    location.href = "/sign-up";
+  });
+  // Return to login page
+  $("#login").click(() => {
+    location.href = "/";
+  });
 
-    // Hide/Show sections based on user nav selection in dashboard
-    // Hide all and only show dashboard upon page load
-    $(".side-navs").hide();
-    $("#dashboard").show();
-    // Hide all and only show user selection
-    // 1000 timer for smooth animation
-    $("#home-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#dashboard").show(1000);
-    });
-    $("#explore-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#explore").show(1000);
-    });
-    $("#messages-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#messages").show(1000);
-    });
-    $("#post-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#post").show(1000);
-    });
-    $("#profile-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#profile").show(1000);
-    });
-    $("#settings-btn").click(function () {
-        $(".side-navs").hide(1000);
-        $("#settings").show(1000);
-    });
+  // Hide/Show navigation tab on user click in dashboard
+  $("#open-nav").click(() => {
+    $("#mySidenav").css("width", "250px");
+  });
+  $(".close-btn").click(() => {
+    $("#mySidenav").css("width", "0px");
+  });
 
-
-
-
-
+  // Hide/Show sections based on user nav selection in dashboard
+  // Hide all and only show dashboard upon page load
+  $(".side-navs").hide();
+  $("#dashboard").show();
+  // Hide all and only show user selection
+  // 500 timer for smooth animation
+  $("#home-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#dashboard").show(500);
+  });
+  $("#explore-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#explore").show(500);
+  });
+  $("#messages-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#messages").show(500);
+  });
+  $("#post-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#post").show(500);
+  });
+  $("#profile-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#profile").show(500);
+  });
+  $("#settings-btn").click(() => {
+    $(".side-navs").hide(500);
+    $("#settings").show(500);
+  });
+  // 
+  $(".boldText").click(() => {
+    $("#textarea").toggleClass("bold");
+  });
+  $(".italicText").click(() => {
+    $("#textarea").toggleClass("italic");
+  });
+  $(".underlineText").click(() => {
+    $("#textarea").toggleClass("underline");
+  });
 });
-
-
